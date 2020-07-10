45f0ac57fc27de11a89493e397e678bf852b9c6a
Jonathan Nilsson <jonathan@voysys.se>
2020-07-11 00:59:22 +0200

    Draw

--- frontend/src/lib.rs
+++ frontend/src/lib.rs
@@ -1,5 +1,6 @@
 use wasm_bindgen::{closure::Closure, prelude::wasm_bindgen, JsCast, JsValue};
 use web_sys::HtmlSpanElement;
+use std::{cell::Cell, rc::Rc};
 
 #[wasm_bindgen(start)]
 pub fn start() -> Result<(), JsValue> {
@@ -22,8 +23,14 @@ pub fn start() -> Result<(), JsValue> {
     div_style.set_property("top", "40px")?;
     div_style.set_property("left", "40px")?;
 
+    let down = Rc::new(Cell::new(false));
+
     {
+        let div = div.clone();
         let div_style = div_style.clone();
+        let inner_document = document.clone();
+        let down = down.clone();
+
         let closure = Closure::wrap(Box::new(move |event: web_sys::MouseEvent| {
             div_style
                 .set_property("left", &format!("{}px", event.client_x()))
@@ -31,31 +38,32 @@ pub fn start() -> Result<(), JsValue> {
             div_style
                 .set_property("top", &format!("{}px", event.client_y()))
                 .unwrap();
+
+            if down.get() {
+                let new_div = div.clone_node().unwrap();
+                new_div.dyn_ref::<HtmlSpanElement>().unwrap().set_inner_text(" ");
+
+                inner_document.body().unwrap().append_child(&new_div).unwrap();
+            }
+
         }) as Box<dyn FnMut(_)>);
         document.add_event_listener_with_callback("mousemove", closure.as_ref().unchecked_ref())?;
         closure.forget();
     }
 
     {
-        let div = div.clone();
-        let inner_document = document.clone();
+        let down = down.clone();
         let closure = Closure::wrap(Box::new(move |_: web_sys::MouseEvent| {
-            div.set_class_name("badge badge-primary");
-
-            let new_div = div.clone_node().unwrap();
-            new_div.dyn_ref::<HtmlSpanElement>().unwrap().set_inner_text(" ");
-
-            inner_document.body().unwrap().append_child(&new_div).unwrap();
-
+            down.set(true);
         }) as Box<dyn FnMut(_)>);
         document.add_event_listener_with_callback("mousedown", closure.as_ref().unchecked_ref())?;
         closure.forget();
     }
 
     {
-        let div = div.clone();
+        let down = down.clone();
         let closure = Closure::wrap(Box::new(move |_: web_sys::MouseEvent| {
-            div.set_class_name("badge badge-primary");
+            down.set(false);
         }) as Box<dyn FnMut(_)>);
         document.add_event_listener_with_callback("mouseup", closure.as_ref().unchecked_ref())?;
         closure.forget();
